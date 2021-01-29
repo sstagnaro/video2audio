28963c4e23f35beb5232c0d109463941b9f66e8e
Stefano Stagnaro <sstagnaro@extraordy.com>
2021-01-29 23:24:27 +0100

    Multiple changes Parse all arguments with "$@" Generate audio file name with Bash string manipulation

--- video2audio.sh
+++ video2audio.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Converte tutti i video nella cartella corrente
-# dal formato video $VIDEOEXT al formato audio $AUDIOEXT
+# Converts all video passed as argument
+# from the current video format to the $AUDIOEXT audio format
 
-VIDEOEXT="mp4"
 AUDIOEXT="mp3"
 AUDIORATE="128K"
 MIOLOGLV="quiet"
 
-for CONTA in {01..10}; do
-	MIOVIDEO=$(ls $CONTA*.mp4)
-	ffmpeg -loglevel $MIOLOGLV -i "${MIOVIDEO}" -b:a $AUDIORATE -vn "$(basename "${MIOVIDEO}" .$VIDEOEXT)".$AUDIOEXT &
+for MIOVIDEO in "$@"; do
+	ffmpeg -loglevel ${MIOLOGLV} -i "${MIOVIDEO}" -b:a ${AUDIORATE} -vn "${MIOVIDEO%.*}.$AUDIOEXT" &
 done
